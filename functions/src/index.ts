e2fb411a2270486b1db44b879dd1b24d9da6b3d0
sesta <sesta.ec5@gmail.com>
2019-01-26 20:51:58 +0900

    最初のセッションが始まる時間を返せるようにする

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,10 +4,16 @@ import {
 } from 'actions-on-google'
 import { https } from 'firebase-functions'
 
+import { getNextSessionTimeMessage } from './nextSession'
+
 const app = dialogflow()
 
 app.intent('Welcome', async(conv: DialogflowConversation) => {
   conv.ask('Droid Kaigi 2019です。知りたいことは何ですか？')
+})
+
+app.intent('NextSessionTime', async(conv: DialogflowConversation) => {
+  conv.ask(getNextSessionTimeMessage())
 })
 
 exports.fulfillment = https.onRequest(app)
---
+++ functions/src/nextSession.ts
@@ -0,0 +1,4 @@
+export function getNextSessionTimeMessage(): string {
+  // TODO: 時間に合わせてメッセージが変わるようにする
+  return '最初のセッションは2月7日、10:00からです。'
+}
